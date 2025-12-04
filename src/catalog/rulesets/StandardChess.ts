1abd6cd819dda9087d99b369de31bb3a493d8ef6
ismills24 <isaacmills86@gmail.com>
2025-12-03 20:59:57 -0500

    fix: bad linting on check rules

--- src/catalog/rulesets/StandardChess.ts
+++ src/catalog/rulesets/StandardChess.ts
@@ -5,6 +5,8 @@ import { Piece } from "../pieces/Piece";
 import { CheckRules } from "./CheckRules";
 import { CheckmateCondition } from "./CheckmateCondition";
 import { PlayerColor } from "../../chess-engine/primitives/PlayerColor";
+import { MovementRestrictions } from "../../chess-engine/rules/MovementPatterns";
+import { Vector2Int } from "../../chess-engine/primitives/Vector2Int";
 
 /**
  * Standard chess ruleset with check/checkmate and king safety.
@@ -23,12 +25,36 @@ export class StandardChessRuleSet implements RuleSet {
                 (m) => !CheckRules.wouldMovePutKingInCheck(state, m, piece.owner)
             );
         
-        // Filter out restricted moves
-        const restrictedMoves = state.movementRestrictions || [];
-        const filteredMoves = legalMoves.filter(move => {
-            return !restrictedMoves.some(restriction =>
-                restriction.restrictedSquares.some(sq => sq.equals(move.to))
-            );
+        // Filter out restricted moves from pieces and tiles
+        const restrictedSquares = new Set<string>();
+        
+        // Collect restrictions from all pieces
+        for (const p of state.board.getAllPieces()) {
+            if (p.getRestrictedSquares) {
+                const restriction = p.getRestrictedSquares(state);
+                if (restriction && restriction.restrictedSquares) {
+                    for (const sq of restriction.restrictedSquares) {
+                        restrictedSquares.add(`${sq.x},${sq.y}`);
+                    }
+                }
+            }
+        }
+        
+        // Collect restrictions from all tiles
+        for (const tile of state.board.getAllTiles()) {
+            if (tile.getRestrictedSquares) {
+                const restriction = tile.getRestrictedSquares(state);
+                if (restriction && restriction.restrictedSquares) {
+                    for (const sq of restriction.restrictedSquares) {
+                        restrictedSquares.add(`${sq.x},${sq.y}`);
+                    }
+                }
+            }
+        }
+        
+        // Filter out moves to restricted squares
+        const filteredMoves = legalMoves.filter((move: Move) => {
+            return !restrictedSquares.has(`${move.to.x},${move.to.y}`);
         });
         
         return filteredMoves;
